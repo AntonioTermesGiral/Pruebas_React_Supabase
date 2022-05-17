abe2aeb30db083b5665503e21384a3a2243068ca
Antonio Termes <antonio.termes@movicoders.com>
2022-05-17 11:50:34 +0200

    Probar eso de profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,7 +3,10 @@ import { createClient } from "@supabase/supabase-js";
 
 // Create a single supabase client for interacting with your database 
 const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhkd3NrdG9ocmh1bHVrcHptaWtlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDk2MDc3MzEsImV4cCI6MTk2NTE4MzczMX0.FK8vTPRkX_ddUd-lijECBpWmLGuFoj7pe89TzvH9Zpk"
-const supabase = createClient('https://hdwsktohrhulukpzmike.supabase.co', key)
+const supabase = createClient('https://hdwsktohrhulukpzmike.supabase.co', key, {
+    persistSession: false,
+    autoRefreshToken: true
+})
 
 type Profile = {
     user_id : string,
